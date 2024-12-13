0a5783579619fdd5cb49bca993db02625b002a30
dntstck <dntstck@gmail.com>
2024-12-13 03:41:18 +0000

    Updated simplecalc.js

--- Week1/simple-calc/simplecalc.js
+++ Week1/simple-calc/simplecalc.js
@@ -1,4 +1,21 @@
 // simplecalc.js
+// dru delarosa
 
-let userInput1 = prompt("enter a number");
+//      pseudo
+//
+//  userinput1 = first number
+//      >
+//  userconditional = add/sub/div/mul
+//      >
+//  userinput2 = second number
+//          >
+//            return result
+//          if not a digit/cmd throw error 
+//                >
+//              ask user for another calculation
+//          >
+//          call function again       
+//          >
+//          if none/exit cmd
+//      exit
 
