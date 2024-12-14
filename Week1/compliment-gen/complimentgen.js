f078f9d0c94e03bcc816535908f7bcfb788eb1c2
dntstck <dntstck@icloud.com>
2024-12-14 15:27:30 +0000

    complimentgen.js v2

--- Week1/compliment-gen/complimentgen.js
+++ Week1/compliment-gen/complimentgen.js
@@ -1,16 +1,4 @@
 // Compliment Generator JS
-//
-// let buttonClick = document.getElementById('clickgen').addEventListener('click',
-//   function() {
-//
-//        alert('Here\'s a compliment you beautiful thing!');
-//    });  // links a defined variable to the button with the id "clickgen"
-//         // adds an event listener when button is clicked
-//         // provides an alert to the user
-//
-
-
-// v2
 
 const niceMessages = [
     "You're great at everything you do.",
@@ -18,14 +6,27 @@ const niceMessages = [
     "Have a great day",
     "Here\'s a compliment you beautiful thing",
     "Another compliment? Oh go on then."
-]
+];
 
-let currentMsg = 0;
+function getInput(){
+    let userName = prompt("Tell me your name: ");
+    let userAge = parseInt(prompt("Whats your age?"));
+    let userHobby = prompt("Favourite hobby?");
+    let userAnimal = prompt("Favourite animal?")
 
-let buttonClick = document.getElementById('clickgen').addEventListener('click',
-    function newMsg(){
-    alert(niceMessages[currentMsg]);
+    if (confirm("Correct?")){
+        alert(`Hi ${userName}, you're ${userAge}. Your hobby is ${userHobby} & your favourite animal is ${userAnimal}`)
+        console.log(`name: ${userName}\n age: ${userAge}\n hobby: ${userHobby}\n animal: ${userAnimal}\n`)
+        alert(`PS: ${niceMessages[0]}`)
+    }
+}
 
-    currentMsg = (currentMsg + 1) % niceMessages.length;
-});
+function runScript(){
+    if (confirm("Run script?")){
+        getInput();
+    } else {
+        alert("Script aborted");
+    }
+}
 
+runScript(); 
