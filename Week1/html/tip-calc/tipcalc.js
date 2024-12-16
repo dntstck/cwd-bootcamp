a9c881efb0985400ca59bd1b67289b50ce6fc0ea
dntstck <dntstck@icloud.com>
2024-12-16 03:35:59 +0000

    fixes to tipcalc.js

--- Week1/html/tip-calc/tipcalc.js
+++ Week1/html/tip-calc/tipcalc.js
@@ -1,4 +1,4 @@
-// Tip Calc v1
+// Tip Calc v2
 
 
  function getInput(){
@@ -7,19 +7,20 @@
     let totalPayee = parseInt(prompt("Shared by how many?"));
     let tipPct = parseFloat(prompt("What's the tip percentage?\n (0.5 = 5%, 0.10 = 10%, 0.25 = 25%)"));
 
+    if (totalPayee === 1){  
+        let tipTotal = (billTotal * tipPct);
+        let maxTotal = (billTotal + tipTotal);
+        tipTotal = tipTotal.toFixed(2); // enables correct use of decimal points when working with currency
+        maxTotal = maxTotal.toFixed(2);
+        console.log(maxTotal, tipTotal) // log this
+        document.getElementById('maxtotal').innerText = maxTotal
+        document.getElementById('total').innerText = tipTotal; //change html element    
+        document.getElementById('totaleach').innerText = "£" + maxTotal;
 
+        alert(` £${billTotal} divided by 1 person, with a ${tipPct}% tip is £${tipTotal}.\n total amount to pay is £${maxTotal}.`);
+    }
 
-    if (confirm("Correct?")){
-        if (totalPayee === 1){  
-            let tipTotal = (billTotal * tipPct);
-            let maxTotal = (billTotal + tipTotal);
-            tipTotal = tipTotal.toFixed(2); // enables correct use of decimal points when working with currency
-            maxTotal = maxTotal.toFixed(2);
-            console.log(maxTotal, tipTotal) // log this
-            document.getElementById('maxtotal').innerText = maxTotal
-            document.getElementById('total').innerText = tipTotal; //change html element
-        alert(` £${billTotal} divided by 1 person, with a ${tipPct}% tip is £${tipTotal}.\n total amount to pay is £${maxTotal}.`);}
-        if (totalPayee > 1){
+    if (totalPayee > 1){
             let tipTotal = (billTotal * tipPct) / totalPayee;
             let maxTotal = (billTotal + tipTotal);
             let perPerson = (tipTotal / totalPayee);
@@ -27,6 +28,7 @@
             tipTotal = tipTotal.toFixed(2);
             maxTotal = maxTotal.toFixed(2);
             perPerson = perPerson.toFixed(2);
+            perPerson = paymentPer.toFixed(2);
             console.log("bill: £", billTotal, "tip pct: " , tipPct, "tip: £", tipTotal, "total: £", maxTotal, "per person: £", perPerson);
             document.getElementById('maxtotal').innerText = "£" + maxTotal 
             document.getElementById('total').innerText = "£" + perPerson;
@@ -34,21 +36,6 @@
             alert(`£${billTotal} divided by ${totalPayee} people, with a ${tipPct}% tip is: £${tipTotal}. \n total amount to pay is: £${maxTotal} \n the tip per person is: £${perPerson}.\n amount required per person is: £${perPerson}`);
         }
     }
-}
 
-function runScript(){
-    if (confirm("Run Tip Calculator?")){
-        getInput();
-    } else {
-        null
-    }
-}
-
-// v2
-
-window.onload = () =>  // arrow function to call the function when btn is clicked
-    {
-        document.querySelector('#calc').onclick = runScript;
-    }
 
 
