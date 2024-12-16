15b4fb9c1775268322d2207bdbd7b7f6d25bddb0
dntstck <dntstck@icloud.com>
2024-12-16 17:39:42 +0000

    fixed links s

---
+++ Week1/html/shop/shop.js
@@ -0,0 +1,39 @@
+// shopping list
+
+function Shoppingitem(name, price, inCart=false){
+    this.name = name;
+    this.price = price;
+    this.inCart = inCart;
+}
+
+async function getShoppingList(){
+    const response = await fetch("https://fakestoreapi.com/products/");
+    const shopItems = await response.json();
+    const outputItems = [];
+    console.log(response)
+
+    for (let i = 0; i < shopItems.length; i++){
+
+        const item = new Shoppingitem(shopItems[i].title, Shoppingitem[i].price);
+        outputItems.push(item);
+    } 
+
+    return outputItems.push(item)
+
+
+}
+
+function dailyImg() {
+    fetch("https://picsum.com/500/500")
+    .then((res) => {
+        console.log(res.url);
+        const newImg = document.getElementById('dailyimg');
+        newImg.setAttribute("src", res.url);
+    });
+}
+
+
+//const item = new Shoppingitem("laptop", "1500");
+
+dailyImg();
+getShoppingList();
